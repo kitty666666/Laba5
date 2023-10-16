9c3fb0734f549718448318164034804e94089c11
kitty666666 <skatusha04@gmail.com>
2023-10-16 16:49:38 +0300

    First commit

---
+++ labochka5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите первое число: ");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число: ");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        // Найдем среднее значение двух чисел
+        double average = (number1 + number2) / 2.0;
+
+        Console.WriteLine("Среднее значение: " + average);
+    }
+}
