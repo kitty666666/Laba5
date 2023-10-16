8fbccff98cd6716322839e146e5bca250e56b9ee
kitty666666 <skatusha04@gmail.com>
2023-10-16 16:52:36 +0300

    Bad commit

--- labochka5/Program.cs
+++ labochka5/Program.cs
@@ -5,10 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Введите первое число: ");
-        double number1 = Convert.ToDouble(Console.ReadLine());
+        double number1 = Convert.ToDouble(); // Ошибка: Не указан аргумент
 
         Console.WriteLine("Введите второе число: ");
-        double number2 = Convert.ToDouble(Console.ReadLine());
+        double number2 = Convert.ToDouble(); // Ошибка: Не указан аргумент
 
         // Найдем среднее значение двух чисел
         double average = (number1 + number2) / 2.0;
